736a4913cb3c3256f7827283cc3e5ec622a957bd
jhersbo <jhersbo@gmail.com>
2022-11-07 22:14:58 -0600

    update readme

--- routes/user_auth.js
+++ routes/user_auth.js
@@ -5,7 +5,7 @@ const { user_auth, user_orders, subscriptions, inventory } = db
 //bcrypt
 
 const bcrypt = require('bcrypt')
-const saltRound = 10
+const saltRounds = 10
 
 //get all users
 router.get('/', async (req, res)=>{
@@ -39,6 +39,28 @@ router.get('/:user_id', async (req, res)=>{
         res.status(500).json(err)
         console.log(err)
     }
+})
+
+//create a new user **untested**
+router.post('/', (req, res)=>{
+    bcrypt.hash(req.body.password_hash, saltRounds, async (err, hash)=>{
+        try{
+            await user_auth.create({
+                user_id: req.body.user_id,
+                email_address: req.body.email_address,
+                password_hash: hash,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name
+            })
+            if(err){
+                res.status(401).json({bcrypt_error_msg: err})
+                return
+            }
+            res.status(200).json("User created.")
+        }catch(err){
+            res.status(500).json("Error creating user.", err)
+        }
+    })
 })
 
 module.exports = router
