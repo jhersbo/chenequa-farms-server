660f2bec0b3223ee55eb8335de468fa08ceb214b
jhersbo <jhersbo@gmail.com>
2022-11-09 11:46:58 -0600

    user_orders done

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 // import cookieSession from 'cookie-session'
-
 const app = express()
-
 dotenv.config()
 
 // app.use(cookieSession({
@@ -21,11 +19,10 @@ app.use(cors({
 
 //parser for request bodies
 app.use(bodyParser.json())
-
-
 //controllers
 app.use('/user_auth', require('./routes/user_auth'))
 app.use('/inventory', require('./routes/inventory'))
+app.use('/user_orders', require('./routes/user_orders'))
 
 app.get('/', async (req, res)=>{
     try{
---
+++ routes/user_orders.js
@@ -0,0 +1,111 @@
+const router = require('express').Router()
+const db = require('../models')
+const { user_auth, user_orders, subscriptions, inventory } = db
+
+//get all orders
+router.get('/', async (req, res)=>{
+    try{
+        let found = await user_orders.findAll({})
+        res.status(200).json(found)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//get all unfilled orders
+router.get('/unfilled', async (req, res)=>{
+    try{
+        let found = await user_orders.findAll({
+            where: {
+                filled: false
+            }
+        })
+        res.status(200).json(found)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//get orders by user
+router.get('/:user_id', async (req, res)=>{
+    try{
+        let found = await user_orders.findAll({
+            where: {
+                user_id: req.params.user_id
+            }
+        })
+        res.status(200).json(found)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//get unfilled user orders
+router.get('/unfilled/:user_id', async (req, res)=>{
+    try{
+        let found = await user_orders.findAll({
+            where: {
+                user_id: req.params.user_id,
+                filled: false
+            }
+        })
+        res.status(200).json(found)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//create a new order
+router.post('/', async (req, res)=>{
+    try{
+        await user_orders.create(req.body)
+        res.status(200).json("Order created.")
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//make edits to an order
+router.put('/', async (req, res)=>{
+    try{
+        let found = await user_orders.findOne({
+            where: {
+                order_id: req.body.order_id
+            }
+        })
+        if(!found){
+            res.status(203).json("No order by that ID exists.")
+        }else{
+            await user_orders.update(req.body, {
+                where: {
+                    order_id: req.body.order_id
+                }
+            })
+            res.status(200).json("Order updated.")
+        }
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//delete an order
+router.delete('/:order_id', async (req, res)=>{
+    try{
+        let found = await user_orders.findOne({
+            where: {
+                order_id: req.params.order_id
+            }
+        })
+        if(!found){
+            res.status(203).json("No order by that ID exists.")
+        }else{
+            await found.destroy()
+            res.status(200).json("Order deleted.")
+        }
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+module.exports = router
