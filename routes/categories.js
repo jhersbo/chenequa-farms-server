e213726055ed9277e7a87168bc8a12943cb66934
jhersbo <jhersbo@gmail.com>
2022-11-28 11:55:07 -0600

    added new route

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
 const db = require('../models')
-const { categories } = db
+const { categories, inventory } = db
 
 router.get('/', async (req, res)=>{
     try{
-        let found = await categories.findAll({})
+        let found = await categories.findAll({
+            include: [
+                {
+                    model: inventory
+                }
+            ]
+        })
         res.status(200).json({
             success: true,
             data: found
@@ -14,6 +20,37 @@ router.get('/', async (req, res)=>{
             success: false,
             error: err,
             message: "Error retrieving categories."
+        })
+    }
+})
+
+router.get('/:category_id', async (req, res)=>{
+    try{
+        let found = await categories.findOne({
+            where: {
+                category_id: req.params.category_id
+            },
+            include: [
+                {
+                    model: inventory
+                }
+            ]
+        })
+        if(!found){
+            return res.status(203).json({
+                success: false,
+                message: "Invalid category ID."
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            data: found
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            error: err,
+            message: "Error retrieving category's inventory."
         })
     }
 })
